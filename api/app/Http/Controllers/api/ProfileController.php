21d2dc2f00e65357f58ae90e85e4f3c7b8077174
kedarcoding <kedargolande9@gmail.com>
2025-02-15 19:47:03 +0530

    Initial project setup with Laravel API & Next.js frontend

---
+++ api/app/Http/Controllers/api/ProfileController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Profile;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request)
+    {
+       $profile= Profile::find($request->user()->id);
+       if(!empty($profile)){
+        return response()->json(['data'=>$profile],200);
+       }
+        return response()->json(['data'=>[],'msg'=>'No Data'],201);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ api/routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\api\ProfileController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('/profile', ProfileController::class);
+});
+
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:api');
+Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
