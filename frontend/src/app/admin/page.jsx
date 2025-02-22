63cf08932ded50585b3164133cd4172e80325041
kedarcoding <kedargolande9@gmail.com>
2025-02-22 14:56:10 +0530

    admin-layout

--- frontend/src/app/admin/page.jsx
+++ frontend/src/app/admin/page.jsx
@@ -1,24 +1,12 @@
 import AdminLayout from "../components/Admin/AdminLayout"
+import Dashborad from "../components/Admin/Dashboard";
 
 
 export default function AdminPage() {
  
   return (
          <AdminLayout>
-          <div className="text-blue-200 mx-2">
-            <p>
-              This is some dummy content for testing the layout. You can replace this with your actual content later.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia, nulla eu auctor tincidunt, orci libero mollis eros, id fringilla lorem tortor nec nunc. Integer ac mi erat. Nunc interdum urna ac purus sollicitudin malesuada. Curabitur aliquet, dui nec tempor venenatis, nisi augue mollis lorem, ac volutpat risus leo ac erat.
-            </p>
-            <p>
-              Proin suscipit risus id leo volutpat, sit amet posuere lorem pharetra. Vivamus porttitor purus ut nisi fermentum, eget posuere tortor varius. Nam et eros at elit ullamcorper dapibus. Aenean vestibulum dolor eget justo iaculis, vel convallis leo auctor. Donec dignissim augue vel justo luctus, vel venenatis leo malesuada.
-            </p>
-            <p>
-              Phasellus malesuada nulla ut mauris tincidunt, et fermentum purus ultricies. Ut auctor nec enim vitae sollicitudin. Integer ultricies, nulla a fermentum condimentum, dui nulla tristique ante, vel tincidunt elit elit ac risus.
-            </p>
-          </div>
+          <Dashborad/>
          </AdminLayout>
   );
 }
--- frontend/src/app/components/Admin/SideBar.js
+++ frontend/src/app/components/Admin/SideBar.js
@@ -9,7 +9,7 @@ const SideBar=()=>{
       ];
 
 return (
-<aside className="overflow-x-hidden text-center bg-gray-200 w-[120px] text-center transition-all duration-300 hover:w-[200px] shadow-lg">
+<aside className="overflow-x-hidden text-center bg-gray-200 w-[200px] text-center transition-all duration-300 hover:w-[280px] shadow-lg">
 <ul className="list-none p-4 flex-grow mx-auto h-[92%] overflow-y-auto">
   {sideMenu.map((item, index) => (
     <li key={index} className="mb-4">
@@ -21,8 +21,8 @@ return (
 </ul>
 {/* Button at the bottom */}
 <div className="border-t-2 border-gray-400">
-<button className="bg-blue-400 text-white mx-1 my-2 p-2 mb-4 rounded-md hover:bg-blue-600 ">
-  Button
+<button className="bg-gray-600 text-white mx-1 my-2 p-2 mb-4 rounded-md hover:bg-gray-700 ">
+  Toggle
 </button>
 </div>
 {/* <div className="border-t-2 border-gray-700 my-1 start-end">
