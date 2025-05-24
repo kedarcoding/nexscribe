fc6c3927bd5d6744c777294d3770ed298550b23c
kedarcoding <kedargolande9@gmail.com>
2025-05-24 18:18:43 +0530

    profile-pic sets

--- frontend/src/app/components/Navbar.js
+++ frontend/src/app/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
       <div className="relative right-20">
         {authUser ? (
           <div className="group relative">
-            <img src={authUser.photo} alt="IMG" className="w-12 h-12 rounded-full cursor-pointer" />
+            <img src={authUser.photo || '/user.png'}alt="IMG" className="w-12 h-12 object-cover rounded-full cursor-pointer" />
             <div className="absolute right-0 mt-0.5 w-28 bg-white shadow-lg rounded-lg hidden group-hover:flex">
               <button onClick={logout} className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200">
                 Logout
