69e1ed02fa8d8f1f06b45be4155919e95b1f431c
kedarcoding <kedargolande9@gmail.com>
2025-05-24 18:12:54 +0530

    some changes

--- frontend/src/app/insights/page.jsx
+++ frontend/src/app/insights/page.jsx
@@ -2,15 +2,26 @@ import InsightComponent from '../components/Insights/InsightComponent';
 // import BlogComponent from '../components/BlogComponent';
 import GuestLayout from '../components/layouts/GuestLayout';
 import ProtectedRoute from '../components/ProtectedRoute';
+import Profile from '../components/User/ProfilePage';
+import Link from 'next/link';
 
-
-export default function Profile() {
+export default function Insights() {
     return (
       <ProtectedRoute>
       <GuestLayout>
-        <div>
-          <InsightComponent/>
-        </div>
+       <div className="flex w-full">
+  {/* Left side: InsightComponent in 3/4 width */}
+  <div className="w-3/4">
+    <InsightComponent />
+  </div>
+
+  {/* Right side: Profile in remaining 1/4 width */}
+  <div className="w-1/4 mt-4">
+  <div>
+    <Link href="/profile" className='button bg-gray-400 text-md font-medium py-1 px-2 text-white border-rounded rounded-md'>Edit Profle</Link></div>
+    <Profile />
+  </div>
+</div>
       </GuestLayout>
       </ProtectedRoute>
     );
